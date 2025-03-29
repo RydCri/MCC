bdae59f7726e92441a7547a6377e0930cb349311
RydCri <107228115+RydCri@users.noreply.github.com>
2025-03-29 01:57:35 -0500

    start stop methods

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,33 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void start(){
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected.");
+                ClientHandler clientHandler = new ClientHandler(socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            if (serverSocket != null){
+                serverSocket.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
